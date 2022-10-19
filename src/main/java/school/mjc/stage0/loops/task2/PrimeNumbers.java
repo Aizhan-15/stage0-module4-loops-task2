000f76523b15557bf1bc9fcdbf5cfea4ea66968f
Aizhan-15 <aizhanurman@gmail.com>
2022-10-19 21:22:34 +0600

    completed the tasks

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
-        int counter = 0;
+        int counter = 2;
         while (counter <= printToInclusive) {
-            System.out.println(counter);
+            System.out.println(counter++);
             counter++;
         }
 
